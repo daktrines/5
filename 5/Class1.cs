0a976487c55cdb2553f82cec4c817deb2366f0a7
ИСП-31 <-31@LAB17-01>
2021-10-29 11:42:29 +0300

    Добавьте файлы проекта.

---
+++ 5/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _5
+{
+    /// <summary>
+    /// класс машина
+    /// </summary>
+    public class Car
+    {
+        //string _marka;//марка автомобиля (Поля)
+        int _culindr; //количество циллиндров
+        int _moshnost; //мощность
+        //метод 
+        public bool Proverka(int value)
+        {
+
+            if (value > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        //Свойства
+        public string Marka { get; set; }//марка автомобиля
+        public int Culindr//количество циллиндров
+        {
+            get
+            {
+                return _culindr;
+            }
+            set
+            {
+                if(Proverka(value) == true)
+                {
+                    _culindr = value;
+                }
+                else MessageBox.Show("Неверные данные!", "Ошибка");
+            }
+        }
+        public int Moshnost //мощность
+        {
+            get
+            {
+                return _moshnost;
+            }
+            set
+            {
+                if (Proverka(value) == true)
+                {
+                    _moshnost = value;
+                }
+                else MessageBox.Show("Неверные данные!", "Ошибка");
+            }
+        
+        }
+
+       
+
+        public string Show()
+        {
+            string info = "Машина имеет такую характеристику:\n" +
+                Marka + " - Марка\n" +
+                Culindr + " - Циллиндр\n" +
+                Moshnost + " - Мощность\n";
+            return info;
+
+        }
+        public Car() { }
+
+        //Параметризированный конструктор 
+        public Car(string initMarka, int initCulindr, int initMashnost)
+        {
+            Marka = initMarka;
+            Culindr = initCulindr;
+            Moshnost = initMashnost;
+        }
+        //перегруженный метод
+        public void SetParams(string marka)
+        {
+            Marka = marka;
+           
+        }
+        public void SetParams(int culindr, int moshnost)
+        {
+            Culindr = culindr;
+            Moshnost = moshnost;
+        }
+        public void SetParams(string marka, int culindr, int moshnost)
+        {
+            Marka = marka;
+            Culindr = culindr;
+            Moshnost = moshnost;
+        }
+
+
+    }
+}
---
+++ 5/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Калион Екатерина Максимовна. 5 пр. Создать базовый класс Саr (машина), характеризуемый торговой маркой (строка), " +
+                "числом цилиндров, мощностью.Создать необходимые методы и свойства.Создать " +
+                " перегруженные методы SetParams, для установки параметров автомобиля.");
+        }
+
+        //Выход из программы
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+     
+       
+        Car car = new Car(); //Создаем обьект типа Car
+        
+        private void Купить_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (int.TryParse(Cilindr.Text, out int cilindr) && int.TryParse(Moshnost.Text, out int moshnost) && cilindr > 0 &&   moshnost > 0)
+            {
+                //если нажали на бмв
+                if (BMW.IsChecked == true)
+                {
+                    car.Marka = BMW.Content.ToString();// используем свойство 
+                }
+                else car.Marka = Audi.Content.ToString();
+                car.SetParams(cilindr, moshnost); // использууем перегруженный метод
+                car1.Text = car.Show(); //используем метод
+            }
+            else MessageBox.Show("Неверные данные!", "Ошибка");
+            
+        }
+
+        private void BMW_Checked(object sender, RoutedEventArgs e)
+        {
+            car.SetParams(Convert.ToString(BMW.Content));// испоьзуем метод для марки
+            Cilindr.Text = "6";
+            Moshnost.Text = "297";
+        }
+        private void Audi_Checked(object sender, RoutedEventArgs e)
+        {
+            car.SetParams(Convert.ToString(Audi.Content));// используем метод
+            Cilindr.Text = "4";
+            Moshnost.Text = "176";
+        }
+    }
+}
+       
+    
+
